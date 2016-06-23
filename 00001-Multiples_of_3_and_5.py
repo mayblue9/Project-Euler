1dad8fc67d9a8e7c2858642ee0f05a467bea1d5d
Stephen Chu <stchu777@gmail.com>
2016-06-23 10:49:04 -0700

    Added timed execution function

--- 00001-Multiples_of_3_and_5.py
+++ 00001-Multiples_of_3_and_5.py
@@ -5,6 +5,14 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 #
+import time
+
+def time_execution(code):
+    start = time.clock()
+    result = eval(code)
+    run_time = time.clock() - start
+    return result, run_time
+
 def problem00001(n):
 	sum = 0
 	for i in range(1,1000):
@@ -12,4 +20,4 @@ def problem00001(n):
 			sum += i
 	return sum
 
-print(problem00001(1000))
+print(time_execution("problem00001(1000)"))
